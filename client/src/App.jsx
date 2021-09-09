47efb8f41b4021e7a464ad7c2d66d688ed49080a
Tristan <tristanshawnjohnson@gmail.com>
2021-09-08 21:51:01 -0500

    I created two different CSS ID's for my list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,14 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      listName: 'flexRow'
     }
   }
 
 
   render () {
     return (
-      <ul>
+      <ul Id={this.state.listName}>
       <li>Item 1</li>
       <li>Item 2</li>
       <li>Item 3</li>
